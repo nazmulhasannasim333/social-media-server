64687bed3c628ee4f5c6dfe0e6bc3cc88cf48c9f
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-03-18 19:34:34 +0600

    add origin url

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://nh-social-media.vercel.app"],
     credentials: true,
   })
 );
